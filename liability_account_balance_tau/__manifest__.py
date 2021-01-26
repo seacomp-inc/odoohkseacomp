aa1d5019f18cacee352e7ec97c1b2839c782e57b
tau-odoo <tau@odoo.com>
2021-01-26 17:07:34 +0800

    [ADD] liability_account_balance_tau

---
+++ liability_account_balance_tau/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+'name': 'liability_account_balance_tau',
+'version': '12.0.1.0',
+'author': 'OdooHK-TAU',
+'description': 'liability account balance',
+'depends': ['account'],
+'data': [
+    'views/views.xml',
+    ],
+}
